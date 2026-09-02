2c29675f6f671b88f9d7b05eb325dc9ec7ac76d4
aalemustafa070-cmd <aalemustafa070@gmail.com>
2026-09-02 11:33:58 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='winter-arc-v37';
-const ASSETS=['./','./Winter_Arc_OS_V3_3.html','./manifest.json','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{let copy=r.clone();caches.open(CACHE).then(x=>x.put(e.request,copy));return r}).catch(()=>caches.match('./Winter_Arc_OS_V3_3.html'))))});
